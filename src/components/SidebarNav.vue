1b155c59aa6c24e7c067bf83aff9522a37b31b5c
Riajul Kashem <riajul.programmer@gmail.com>
2021-04-20 22:34:04 +0600

    SidebarNav menu item designed

--- src/components/SidebarNav.vue
+++ src/components/SidebarNav.vue
@@ -1,6 +1,12 @@
 <template>
-  <div class="lg:space-x-0 flex lg:flex-col items-center justify-center h-full lg:space-y-4 py-4">
-        <a href="#" class="block w-14 h-14 bg-white rounded-xl mx-3"></a>
+  <div class="lg:space-x-0 flex lg:flex-col items-center justify-center h-full lg:space-y-4 lg:py-4 py-2">
+        <a href="#" class="group">
+          <div class="sidebar-menu-item">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" viewBox="0 0 20 20" fill="currentColor">
+              <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" />
+            </svg>
+          </div>
+        </a>
         <a href="#" class="block w-14 h-14 bg-white rounded-xl mx-3"></a>
         <a href="#" class="block w-14 h-14 bg-white rounded-xl mx-3"></a>
         <a href="#" class="block w-14 h-14 bg-white rounded-xl mx-3"></a>
